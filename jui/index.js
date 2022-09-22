e8433c4b340c6ecaf46e08c95a1bc54c522f7864
Jing-Ying Chen <jy.robin.chen@gmail.com>
2022-09-22 12:25:57 +0800

    save

--- jui/index.js
+++ jui/index.js
@@ -1,6 +1,6 @@
 
 module.exports = Object.assign({
   assetLoader: require('./asset-loader'),
+  Comp: require('./comp'),
 }, require('./comp'), require('./ajax'), require('./loadjs'), require('./dom'),
   require('../lib/glob'), require('../lib/access'), require('../lib/utils'));
-
